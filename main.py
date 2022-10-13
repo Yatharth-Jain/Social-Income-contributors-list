e7c34d012fc21312bfd1194b6637bcc9e5fafad9
Yatharth Jain <yatharthjain41@gmail.com>
2022-10-13 14:14:37 +0530

    required changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-inititate="npm i svg-contributors -g"
+# inititate="npm i svg-contributors -g"
 bashCommand = "dummysvg -o socialincome-san -n public -s 30 -b"
-os.system(inititate)
+# os.system(inititate)
 os.system(bashCommand)
